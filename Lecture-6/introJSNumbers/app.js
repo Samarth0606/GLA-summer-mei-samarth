b8fa05c93d6a73a6d117de83437dc25522d95abb
Samarth0606 <officialdevelopersamarth@gmail.com>
2023-06-14 14:13:16 +0530

    lecture 6 codes

---
+++ Lecture-6/introJSNumbers/app.js
@@ -0,0 +1,40 @@
+
+let num1 = 10;
+let num2 = 10.10;
+let num3 = -199;
+
+
+console.log(num1)
+console.log(num2)
+console.log(num3)
+
+
+console.log(typeof(num3))
+console.log(typeof(num2))
+console.log(typeof(num1))
+
+
+// mathematical operations
+let num4 = num1 * num2
+console.log(num4)
+
+// rule (PEMDAS)--> priority
+// let ans = num1*num2/num3+num2-num4
+
+//** (power)
+let ans = ( 1*2 - (3-4) + 3**2/2 )
+console.log(ans); //7.5
+
+
+
+
+
+
+
+
+
+
+
+
+
+
